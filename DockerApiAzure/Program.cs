fd1395102db44cd77065b5273d108bc38e8bb584
Johnjalani <jfrbph12@hotmail.com>
2023-02-17 13:19:49 +0800

    fix: proxy settings

--- DockerApiAzure/Program.cs
+++ DockerApiAzure/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,7 +17,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 app.UseAuthorization();
 
 app.MapControllers();
